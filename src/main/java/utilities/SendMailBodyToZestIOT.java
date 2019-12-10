3381070317abf95ad1324668be8787fd44f2f1d8
suryakiranreddy <rbuddha@enhops.com>
2019-12-10 13:46:38 +0530

    2seperate class created for mail body

--- src/main/java/utilities/SendMailBodyToZestIOT.java
+++ src/main/java/utilities/SendMailBodyToZestIOT.java
@@ -1,31 +1,21 @@
 package utilities;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import enh.db.cases.COBT_For_DIALCelebi_User;
-import enh.db.cases.COBT_For_GMR_HYD_AISATS_User;
-import enh.db.cases.COBT_For_GMR_HYD_SG_User;
 
-public class SendMailBodyToZestIOT extends Utility {
-	public static String mailBody;
- 
-    public static void sendHtmlEmail() throws Exception {
-    	Date date = new Date();
-		SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");  
-		String strDate = formatter.format(date);
-    	
-    	 mailBody="<html>"
-    			+ "<p style=\"color:#008ae6;\">Hi All, <br>Please find attached current sprint <b><i>'"+strDate+" Automation Test Results'</i> </b>triggred by Jenkins.<br><br> "
-				+COBT_For_DIALCelebi_User.email_COBT_For_DIALCelebi_User5.toString()+"<br><br>"
-				+COBT_For_GMR_HYD_AISATS_User.email_COBT_For_DIALCelebi_User5.toString()+"<br><br>"
-				+COBT_For_GMR_HYD_SG_User.email_COBT_For_DIALCelebi_User5.toString()+"<br><br>"
+import enh.db.cases.SQL_Queries;
+
+
+
+public class SendMailBodyToZestIOT  {
+	
+	public static String mailBody="<html>"
+    			+ "<p style=\"color:#008ae6;\">Hi All, <br>Please find attached current sprint <b><i>'"+SQL_Queries.todayDayDateTime()+" Automation Test Results'</i> </b>triggred by Jenkins. "				
     			+" <br><br><br> Thanks & Regards,<br>Automation Team</p>"
     			+ "<html>";
     	
+    	   
     	
     	
-    	
-    }
+    
  
 }
