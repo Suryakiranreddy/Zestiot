6354bb1f97d78e5f48f1c6cb2380c74ca57cb7b9
RadhikaChiluka <rchiluka@enhops.com>
2019-12-10 16:18:33 +0530

    send mail updated

--- src/main/java/utilities/SendMail.java
+++ src/main/java/utilities/SendMail.java
@@ -24,6 +24,10 @@ import javax.mail.internet.MimeMultipart;
 import org.apache.commons.io.FileUtils;
 import org.testng.annotations.Test;
 
+import enh.db.cases.COBT_For_DIALCelebi_User;
+import enh.db.cases.COBT_For_GMR_HYD_AISATS_User;
+import enh.db.cases.COBT_For_GMR_HYD_SG_User;
+
 
 
 
@@ -130,7 +134,10 @@ public class SendMail extends Utility {
 		/*messageBodyPart.setText("Hi, \nPlease find attached current sprint Automation Test Results triggred by Jenkins.  "
 				+ " \n \n \nThanks & Regards,\n Automation Team");#00b8e6*/
 		String mailBody="<html>"
-    			+ "<p style=\"color:#008ae6;\">Hi All, <br>Please find attached <b><i>'"+strDate+" Automation Test Results'</i> </b>triggred by Jenkins. "				
+    			+ "<p style=\"color:#008ae6;\">Hi All, <br>Please find attached <b><i>'"+strDate+" Automation Test Results'</i> </b>triggred by Jenkins.<br> "	
+    			+COBT_For_DIALCelebi_User.email_COBT_For_DIALCelebi_User5.toString()
+    			+COBT_For_GMR_HYD_AISATS_User.email_COBT_For_DIALCelebi_User5.toString()
+    			+COBT_For_GMR_HYD_SG_User.email_COBT_For_DIALCelebi_User5.toString()
     			+" <br><br><br> Thanks & Regards,<br>Automation Team</p>"
     			+ "<html>";
     	
