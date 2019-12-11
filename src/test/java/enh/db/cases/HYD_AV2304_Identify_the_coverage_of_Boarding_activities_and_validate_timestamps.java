7eecfd42e61352e14738f1084d93d458db43144c
RadhikaChiluka <58548727+RadhikaChiluka@users.noreply.github.com>
2019-12-11 16:44:36 +0530

    jenkins file update

--- src/test/java/enh/db/cases/HYD_AV2304_Identify_the_coverage_of_Boarding_activities_and_validate_timestamps.java
+++ src/test/java/enh/db/cases/HYD_AV2304_Identify_the_coverage_of_Boarding_activities_and_validate_timestamps.java
@@ -11,6 +11,7 @@ public class HYD_AV2304_Identify_the_coverage_of_Boarding_activities_and_validat
 	public static String TotalFlightsDeparted =null;
 	public static ArrayList<String> list_TotalNumberofPassengerCoaches= new ArrayList<String>();
 	public static ArrayList<String> list_TotalNumberofPassengerCoachesUtilizedforBoarding = new ArrayList<String>();
+	public static ArrayList<String> list_flightswithalltheBoardingactivities  = new ArrayList<String>();
 	
 	@Test
 	public static void hYD_AV2304_Identify_the_coverage_of_Boarding_activities_and_validate_timestamps(int operationUnit, String deviceID) throws SQLException {
@@ -40,7 +41,15 @@ public class HYD_AV2304_Identify_the_coverage_of_Boarding_activities_and_validat
 		
 		System.out.println(list_TotalNumberofPassengerCoachesUtilizedforBoarding.size());		
 		
+		ResultSet result3 = DBWrapper.Connect("SELECT flight_pk FROM `EquipActivityLogs` "
+				+ "where date(flogdate) = '"+SQL_Queries.yesterDate()+"' and devid like '%"+deviceID+"%' and assigned=1 and operationname in ('pcb', 'pcba', 'pcg') group by flight_pk");
+				while (result3.next())
+		{				
+			String	flightPk = result3.getString("flight_pk");
+			list_flightswithalltheBoardingactivities.add(flightPk);	
+		}	
 		
+		System.out.println(list_flightswithalltheBoardingactivities.size());		
 		
 		
 		
