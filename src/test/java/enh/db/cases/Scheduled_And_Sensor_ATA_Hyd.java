afecaee4023e113f3e6705e1e6442007db3fea4c
RadhikaChiluka <rchiluka@enhops.com>
2019-12-11 12:45:29 +0530

    new class created for send mail

--- src/test/java/enh/db/cases/Scheduled_And_Sensor_ATA_Hyd.java
+++ src/test/java/enh/db/cases/Scheduled_And_Sensor_ATA_Hyd.java
@@ -202,7 +202,7 @@ public class Scheduled_And_Sensor_ATA_Hyd {
 			}		
 		}	
 		
-		email_report_Scheduled_And_Sensor_ATA_For_Hyd1.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; color: white;} table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #336600;font-weight: bold;}</style>");
+		email_report_Scheduled_And_Sensor_ATA_For_Hyd1.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; color: #000000;} table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #336600;font-weight: bold;}</style>");
 		email_report_Scheduled_And_Sensor_ATA_For_Hyd1.append("<h4 align=\"center\" style=\"color:#336600;\">Airport Name : GMR-Hyderabad</h4>");
 		email_report_Scheduled_And_Sensor_ATA_For_Hyd1.append("<h4 align=\"center\" style=\"color:#336600;\">Executed For :Scheduled and Sensor-ATA</h4><h5 align=\"center\" style=\"color:#336600;\" >Execution Time: "+SQL_Queries.todayDayDateTime()+" </h5>");
 		email_report_Scheduled_And_Sensor_ATA_For_Hyd1.append("<table style=\"width:100%\" id=\"t01\"><tr><th style=\"width:10%\"><b>Date</b></th><th style=\"width:15%\"><b>Total No. of Flights Scheduled Arrival</b></th>"
