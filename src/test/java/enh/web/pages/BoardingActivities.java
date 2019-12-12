94efc6c5a89cac61f767c470fd7d7722d6d9fbde
RadhikaChiluka <58548727+RadhikaChiluka@users.noreply.github.com>
2019-12-12 18:25:05 +0530

    borading activities code added

--- src/test/java/enh/web/pages/BoardingActivities.java
+++ src/test/java/enh/web/pages/BoardingActivities.java
@@ -15,17 +15,43 @@ public class BoardingActivities extends KeywordUtil {
 	private static By departed = By.xpath("//*[@class='mat-tab-label-content'][contains(.,'Departed')]");
 	static JavascriptExecutor js = (JavascriptExecutor) getDriver();
 	public static int DepatedFlightsCount=0;
-	public static int pushbackAndArrival=0;
-	public static int pushback=0;
-	public static int pushback_NoPBT = 0;
-	public static int PBT_NoPushback=0;
-	public static int pbArrival =0;
+	public static int NumberOfFlightsWithAllTheBoardingActivities=0;
+	public static int NumberOfFlightsWithoutAllTheBoardingActivities=0;
+	public static int NoPCBactivities = 0;
+	public static int withPCBactivitiesButNoPCGactivities=0;
+	public static int withPCBactivitiesButNoPCBAactivities =0;
 	public static int noPBTnoPushback =0;
 	public static int missingFlight=0;
 	public static int j=1;
+	
+	 public static StringBuilder email_BoardingActivities1= new StringBuilder();
+	 public static StringBuilder email_BoardingActivities2= new StringBuilder();	 
+	 public static StringBuilder email_BoardingActivities3= new StringBuilder();
+	 public static StringBuilder email_BoardingActivities4= new StringBuilder();
+	 public static StringBuilder email_BoardingActivities5= new StringBuilder();
 
 	public static void boardingActivities() throws Exception
 	{
+		email_BoardingActivities1.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; color: white;} table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #008ae6;font-weight: bold;}</style>");
+		email_BoardingActivities1.append("<table style=\"width:100%\" id=\"t01\"><h4><caption> Total flights without the Boarding activities (PCG, PCB, and PCBA): </caption></h4>"
+		 		+ "<tr><th style=\"width:25%\">Flight Unique ID</th> <th style=\"width:25%\">Flight Number - Departure Number</th>"
+			 		+ "<th style=\"width:25%\">Departure Date </th></tr>");
+		
+		email_BoardingActivities2.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; color: white;} table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #008ae6;font-weight: bold;}</style>");
+		email_BoardingActivities2.append("<table style=\"width:100%\" id=\"t01\"><h4><caption> Total flights with No PCB activities:  </caption></h4>"
+		 		+ "<tr><th style=\"width:25%\">Flight Unique ID</th> <th style=\"width:25%\">Flight Number - Departure Number</th>"
+			 		+ "<th style=\"width:25%\">Departure Date </th></tr>");
+	
+		email_BoardingActivities3.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; color: white;} table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #008ae6;font-weight: bold;}</style>");
+		email_BoardingActivities3.append("<table style=\"width:100%\" id=\"t01\"><h4><caption>	Total flights with PCB activities but no PCG activities  </caption></h4>"
+		 		+ "<tr><th style=\"width:25%\">Flight Unique ID</th> <th style=\"width:25%\">Flight Number - Departure Number</th>"
+			 		+ "<th style=\"width:25%\">Departure Date </th><th style=\"width:25%\">Operation PCB timestamp: Start & End </th></tr>");
+	
+		email_BoardingActivities4.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; color: white;} table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #008ae6;font-weight: bold;}</style>");
+		email_BoardingActivities4.append("<table style=\"width:100%\" id=\"t01\"><h4><caption>	Total Number of flights with PCB activities but no PCBA activities:   </caption></h4>"
+		 		+ "<tr><th style=\"width:25%\">Flight Unique ID</th> <th style=\"width:25%\">Flight Number - Departure Number</th>"
+			 		+ "<th style=\"width:25%\">Departure Date </th><th style=\"width:25%\">Operation PCB timestamp: Start & End </th></tr>");
+	
 		String userName=getElementText(By.xpath("html/body/app-root/div/app-topnav/mat-toolbar/div[4]/div[2]"));
 		System.out.println(userName);
 		HtmlReportUtil.stepInfo("<b style=\"color:brown;\"> Reports for "+userName+" </b>");
@@ -37,8 +63,7 @@ public class BoardingActivities extends KeywordUtil {
 		click(turnaroundList);
 		delay(2000);
 		click(departed);
-		delay(2000);
-		
+		delay(2000);		
 		//calenderInput("13/NOV/2019");
 		clearInput(By.xpath("//input[@name='date']"));
 		delay(2000);
@@ -67,43 +92,54 @@ public class BoardingActivities extends KeywordUtil {
 				
 						if(flightInfoText.contains("PC Boarding") && (flightInfoText.contains("PC Gate Arrival"))&& (flightInfoText.contains("PCB Arrival ")))
 						{
-							pushbackAndArrival= pushbackAndArrival+1;
-							String flightNum=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							NumberOfFlightsWithAllTheBoardingActivities= NumberOfFlightsWithAllTheBoardingActivities+1;							
 							
 						}
 						if(!flightInfoText.contains("PC Boarding") && (!flightInfoText.contains("PC Gate Arrival"))&& (!flightInfoText.contains("PCB Arrival ")))
 						{
-							pushback_NoPBT = pushback_NoPBT+1;
-							String x1=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							NumberOfFlightsWithoutAllTheBoardingActivities = NumberOfFlightsWithoutAllTheBoardingActivities+1;
+							String flightNum=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							String flightNumm[] =flightNum.split("-");
+							String flightId=getDriver().findElement(By.xpath("(//div[@id='selectedCard']["+i+"]/div/div/div/div)[1]")).getAttribute("title");
+							String deparDate=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/div/div/div[8]/div/div[2]/p[4]"));
+							email_BoardingActivities1.append("<tr><td><b style=\"color:red;\">"+flightNumm[0]+"</b></td><td><b style=\"color:red;\">"+flightId+"</b></td>"
+									+ "<td><b style=\"color:red;\">"+deparDate+"</b></td></tr>");
+
 						}
 						if((!flightInfoText.contains("PC Boarding")))
 						{
-							PBT_NoPushback = PBT_NoPushback+1;
-							String x1=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							NoPCBactivities = NoPCBactivities+1;
+							String flightNum=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							String flightNumm[] =flightNum.split("-");
+							String flightId=getDriver().findElement(By.xpath("(//div[@id='selectedCard']["+i+"]/div/div/div/div)[1]")).getAttribute("title");
+							String deparDate=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/div/div/div[8]/div/div[2]/p[4]"));
+							email_BoardingActivities2.append("<tr><td><b style=\"color:red;\">"+flightNumm[0]+"</b></td><td><b style=\"color:red;\">"+flightId+"</b></td>"
+									+ "<td><b style=\"color:red;\">"+deparDate+"</b></td></tr>");
+
 						}
 						if(flightInfoText.contains("PC Boarding") && (!flightInfoText.contains("PC Gate Arrival")))
 						{
-								pushback= pushback+1;
-								String x1=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							withPCBactivitiesButNoPCGactivities= withPCBactivitiesButNoPCGactivities+1;
+							String flightNum=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							String flightNumm[] =flightNum.split("-");
+							String flightId=getDriver().findElement(By.xpath("(//div[@id='selectedCard']["+i+"]/div/div/div/div)[1]")).getAttribute("title");
+							String deparDate=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/div/div/div[8]/div/div[2]/p[4]"));
+							email_BoardingActivities3.append("<tr><td><b style=\"color:red;\">"+flightNumm[0]+"</b></td><td><b style=\"color:red;\">"+flightId+"</b></td>"
+									+ "<td><b style=\"color:red;\">"+deparDate+"</b></td></tr>");
+
 						}
 						if(flightInfoText.contains("PC Boarding")&& (!flightInfoText.contains("PCB Arrival ")))
 							{
-								pbArrival= pbArrival+1;
-								String x1=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
-							}
-						
-						click(By.xpath("//div[@id='selectedCard']["+i+"]"));
-						delay(2000);
-						try{
-							j=j+1;
-							js.executeScript("arguments[0].scrollIntoView();",getWebElement(By.xpath("//div[@id='selectedCard']["+j+"]")));	
-							delay(3000);
-						}catch(Exception e)
-						{
-							//e.printStackTrace();
-						} 
-						
-						
+							withPCBactivitiesButNoPCBAactivities= withPCBactivitiesButNoPCBAactivities+1;
+							String flightNum=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/descendant::p[1]"));
+							String flightNumm[] =flightNum.split("-");
+							String flightId=getDriver().findElement(By.xpath("(//div[@id='selectedCard']["+i+"]/div/div/div/div)[1]")).getAttribute("title");
+							String deparDate=getElementText(By.xpath("//div[@id='selectedCard']["+i+"]/div/div/div[8]/div/div[2]/p[4]"));
+							email_BoardingActivities4.append("<tr><td><b style=\"color:red;\">"+flightNumm[0]+"</b></td><td><b style=\"color:red;\">"+flightId+"</b></td>"
+									+ "<td><b style=\"color:red;\">"+deparDate+"</b></td></tr>");
+
+							}						
+												
 			}
 			catch(Exception e)
 			{
@@ -113,6 +149,19 @@ public class BoardingActivities extends KeywordUtil {
 			} 
 
 		}
+		
+		email_BoardingActivities5.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; color: #000000;} table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #008ae6;font-weight: bold;}</style>");
+
+		email_BoardingActivities5.append("<h4 align=\"center\" style=\"color:#008ae6;\">Executed For :COBT For DIALCelebi</h4><h5 align=\"center\" style=\"color:#008ae6;\" >Execution Time: "+SQL_Queries.todayDayDateTime()+" </h5>");
+		email_BoardingActivities5.append("<table style=\"width:100%\" id=\"t01\"><tr><th style=\"width:15%\"><b>Date</b></th><th style=\"width:20%\"><b>Total No. of Flights</b></th> <th style=\"width:20%\"><b>COBT  Not detected</b></th>"
+			 		+ "<th style=\"width:25%\"><b>OFFBLOCK not detected</b></th><th style=\"width:20%\"><b>Actual OFFBLOCK & COBT diff  > 5mins</b></th>"
+			 		+ " </tr>");
+	/*	email_BoardingActivities5.append(" <tr> <td><b>"+SQL_Queries.yesterDate()+"</b></td> <td><b>"+cobtTimeDiff.size()+"</b></td>"
+			 		+ " <td> <b style=\"color:red;\">"+cobtIsNotNull.size()+"</b></td> <td><b style=\"color:red;\">"+ofBlockIsNotNull.size()+"</b></td> <td><b style=\"color:red;\">"+str_cobtDiff+"</b></td>  </tr></table>");			 
+*/
+		
+		
+		
 		
 		}	
 		}
