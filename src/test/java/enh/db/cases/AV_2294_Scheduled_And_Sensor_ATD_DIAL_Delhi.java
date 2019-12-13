9acf3d41bb2ab3d8a6dd6aa40a7f05e691000a51
RadhikaChiluka <58548727+RadhikaChiluka@users.noreply.github.com>
2019-12-13 11:34:03 +0530

    2294 changed

--- src/test/java/enh/db/cases/AV_2294_Scheduled_And_Sensor_ATD_DIAL_Delhi.java
+++ src/test/java/enh/db/cases/AV_2294_Scheduled_And_Sensor_ATD_DIAL_Delhi.java
@@ -136,7 +136,7 @@ public class AV_2294_Scheduled_And_Sensor_ATD_DIAL_Delhi {
 				
 				email_report_Scheduled_And_Sensor_ATD_For_Delhi3.append("<br><br>");
 				email_report_Scheduled_And_Sensor_ATD_For_Delhi3.append("<style>table#t01, th, td {border: 1px solid black;border-collapse: collapse;}table#t01 th{background-color:#80e5ff; } table#t01 tr:nth-child(even) {background-color: #f2f2f2;} table#t01 tr:nth-child(odd) { background-color: #DFEDEC;}table#t01 th, td {padding: 5px;}table#t01 th,td {text-align: center;} table#t01 caption {color: #008ae6;font-weight: bold;}</style>");
-				email_report_Scheduled_And_Sensor_ATD_For_Delhi3.append("<table style=\"width:100%\" id=\"t01\"><caption> OFFBLOCK time detected by Flight Sensor greater than AIRBORNE time detected by Flight Sensor</caption><tr
+				email_report_Scheduled_And_Sensor_ATD_For_Delhi3.append("<table style=\"width:100%\" id=\"t01\"><caption> OFFBLOCK time detected by Flight Sensor greater than AIRBORNE time detected by Flight Sensor</caption><tr>"
 						+ "<th style=\"width:10%\"><b>LogID</b></th>"
 						+ "<th style=\"width:15%\"><b>Departure Flight No.</b></th> "
 						+ "<th style=\"width:15%\"><b>Off Block time</b></th> "
